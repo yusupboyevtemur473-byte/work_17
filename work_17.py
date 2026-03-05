395c216bba0627cd30346c17e3d6c9e1c7b6b48c
TEMURBEK <yusupboyevtemur473@gmail.com>
2026-03-05 14:36:50 +0500

    Add files via upload

---
+++ work_17.py
@@ -0,0 +1,70 @@
+#7-misol
+#a = int(input())
+#b = int(input())
+#if a < b:
+#    print("1")
+#else:
+#    print("2")
+    
+#8-misol
+##a = int(input())
+#b = int(input())
+#if a > b:
+#    print(a, b)
+#else:
+#    print(b, a)
+    
+#9-misol
+#a = float(input())
+#b = float(input())
+#if a > b:
+ #   a, b = b, a
+#print("A =", a)
+#print("B =", b)
+
+#10-misol
+#a = int(input())
+#b = int(input())
+#if a != b:
+ #   s = a + b
+  #  a = s
+   # b = s
+#else:
+ #   a = 0
+  #  b = 0
+#print(a, b)
+
+#11-misol
+#a = int(input())
+#b = int(input())
+#if a != b:
+ #   katta = max(a, b)
+  #  a = katta
+   # b = katta
+#else:
+ #   a = 0
+  #  b = 0
+#print(a, b)
+
+#12-misol
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#kichik = a
+#if b < kichik:
+ #   kichik = b
+#if c < kichik:
+ #   kichik = c
+#print(kichik)
+
+#13-misol
+#a = int(input())
+# = int(input())
+#c = int(input())
+#if (a > b and a < c) or (a < b and a > c):
+ #   print(a)
+#elif (b > a and b < c) or (b < a and b > c):
+ #   print(b)
+#else:
+ #   print(c)
+    
